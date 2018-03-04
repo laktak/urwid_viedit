ddd26cae0d7a8f92c86cd72b51e68b6bf038e41e
Christian Zangl <laktak@cdak.net>
2018-03-04 21:30:57 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
-from urwid_viedit import version
+import os
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'urwid_viedit', '__init__.py')) as fd:
+    version = [line.split()[-1].strip("\"") for line in fd if line.startswith('version')]
+if not version: raise Exception("Missing version!")
 
 setup(
     name="urwid_viedit",
     description="Edit widget supporting vi-mode and emacs keybindings",
     long_description=open("README.rst").read(),
     url="https://github.com/laktak/urwid_viedit",
-    version=version,
+    version=version[0],
     author="Christian Zangl",
     author_email="laktak@cdak.net",
     packages=["urwid_viedit"],
--- urwid_viedit/__init__.py
+++ urwid_viedit/__init__.py
@@ -1,3 +1,3 @@
 from .viedit import ViEdit
 
-version = '0.1.0'
+version = "0.1.1"
